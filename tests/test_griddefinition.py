5e0772c423001b2d8ddad64b5813882fec5676c7
Stefan Hendricks <stefan.hendricks@awi.de>
2024-06-18 09:18:36 +0200

    fix tests

--- tests/test_griddefinition.py
+++ tests/test_griddefinition.py
@@ -4,17 +4,17 @@ from pydantic import ValidationError
 
 # Test data for happy path scenarios
 happy_cases_data = [
-    ("valid_epsg_6931", "6931", (0., 100., 0., 100.), 10.),
-    ("valid_epsg_3857", "6931", (-5_000_000., 5_000_000., -5_000_000., 5_000_000.), 10_000.),
+    ("valid_epsg_6931", 6931, (0., 100., 0., 100.), 10.),
+    ("valid_epsg_3857", 6931, (-5_000_000., 5_000_000., -5_000_000., 5_000_000.), 10_000.),
 ]
 
 
 # Test data for error cases (should be catched by input data validation)
 error_cases_data = [
-    ("zero_extent", "6931", (0, 0, 0, 0), 10),
-    ("invalid_epsg", "A", (0, 100, 0, 100), 10),
-    ("negative_resolution", "6931", (0, 100, 0, 100), -10),
-    ("extent_resolution_mismatch", "6931", (-5_000_000., 5_000_000., -5_000_000., 5_000_000.), 3_333.),
+    ("zero_extent", 6931, (0, 0, 0, 0), 10),
+    ("invalid_epsg", "693a", (0, 100, 0, 100), 10),
+    ("negative_resolution", 6931, (0, 100, 0, 100), -10),
+    ("extent_resolution_mismatch", 6931, (-5_000_000., 5_000_000., -5_000_000., 5_000_000.), 3_333.),
 ]
 
 
